0a9e0f727be4c64a6b6cab5c0c868d44c1a006f2
smilv <owou@icloud.com>
2020-10-29 18:43:51 +0800

    红包

--- 红包/index.js
+++ 红包/index.js
@@ -3,7 +3,7 @@
  * @Autor: bin
  * @Date: 2020-10-26 10:25:54
  * @LastEditors: bin
- * @LastEditTime: 2020-10-29 16:47:11
+ * @LastEditTime: 2020-10-29 18:34:49
  */
 (function() {
     $("#main").load("./template.html", function() {
@@ -20,11 +20,17 @@
         // $(".red-pkg-notopen .open")
         //     .delay(3500)
         //     .show(0);
-        // 开启红包
+        //开启红包
         // $(".red-pkg-notopen .open").click(function() {
         $(".red-pkg-notopen").hide();
         $(".red-pkg-open").addClass("red-pkg-open-show");
         $(".red-pkg-close").animate({ marginLeft: "160px" }, 300);
         // });
+        //打开快捷回复列表
+        $(".red-pkg-open .operation .reply").click(function() {
+            $(".red-pkg-open .bg").animate({ top: "-100px" }, 200);
+            $(".red-pkg-close").animate({ marginTop: "-320" }, 200);
+            $(".red-pkg-open .reply-list").addClass("reply-list-show");
+        });
     });
 })();
